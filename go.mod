e3683f8e2929f1cf4e003e0c7f73b260192ccb92
jxsl13 <jxsl13@gmail.com>
2021-06-13 11:02:32 +0200

    use external scheduler library and use always the same msg ID

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/diamondburned/arikawa/v2 v2.1.0
+	github.com/go-co-op/gocron v1.6.2
+	github.com/joho/godotenv v1.3.0
 	github.com/jxsl13/simple-configo v1.4.3
 	github.com/jxsl13/twapi v1.2.1
 )
--- main.go
+++ main.go
@@ -1,36 +1,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/diamondburned/arikawa/v2/bot"
+	"github.com/diamondburned/arikawa/v2/discord"
 	"github.com/diamondburned/arikawa/v2/gateway"
+	"github.com/go-co-op/gocron"
+	"github.com/joho/godotenv"
 	"github.com/jxsl13/TeeworldsServerStatusDiscordBot/markdown"
 	configo "github.com/jxsl13/simple-configo"
 	"github.com/jxsl13/twapi/browser"
 )
 
 var (
-	cfg           = &Config{}
-	bulletinBoard = (*BulletinBoard)(nil)
+	cfg             = &Config{}
+	scheduler       = gocron.NewScheduler(time.UTC)
+	addressMsgIDMap map[string]discord.MessageID
 )
 
 func init() {
-	if err := configo.Parse(cfg, configo.GetEnv()); err != nil {
+	useEnvFile := flag.String("env-file", "", "--env-file .env")
+	flag.Parse()
+
+	env := make(map[string]string)
+	var err error
+
+	if *useEnvFile != "" {
+		env, err = godotenv.Read(*useEnvFile)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		env = configo.GetEnv()
+	}
+
+	if err := configo.Parse(cfg, env); err != nil {
 		log.Fatalln(err)
 	}
 
-	bulletinBoard = NewBulletinBoard(uint64(cfg.DiscordChannelID))
+	scheduler.TagsUnique()
+	addressMsgIDMap = make(map[string]discord.MessageID)
 }
 
 func main() {
 
-	bot.Run(cfg.DiscordToken, &Bot{Cfg: cfg},
+	bot.Run(cfg.DiscordToken, &Bot{Cfg: cfg, AddressMsgIDMap: addressMsgIDMap},
 		func(ctx *bot.Context) error {
 			ctx.HasPrefix = NewRBACPrefix(cfg)
 
@@ -45,39 +67,45 @@ func main() {
 					continue
 				}
 
-				bulletinBoard.Register(address, msg.ID, cfg.RefreshInterval, func() error {
+				channelID := cfg.DiscordChannelID
+				msgID := msg.ID
 
-					address := address
+				addressMsgIDMap[address] = msgID
 
-					channelID := cfg.DiscordChannelID
-					msgID := msg.ID
+				ip, port, err := toIPAndPort(address)
+				if err != nil {
+					return err
+				}
 
-					ip, port, err := toIPAndPort(address)
-					if err != nil {
-						return err
-					}
+				scheduler.
+					Every(cfg.RefreshInterval).
+					Tag(address).
+					Do(
+						func(channelID discord.ChannelID, msgID discord.MessageID, ip string, port int) {
 
-					updateMessage := ""
+							updateMessage := ""
 
-					serverInfo, err := browser.GetServerInfoWithTimeout(ip, port, cfg.RefreshInterval)
-					if err != nil {
-						updateMessage = fmt.Sprintf("%s: %s", markdown.WrapInFat("[DOWN]"), address)
-						log.Println(err)
-					} else {
-						updateMessage = formatServerInfo(serverInfo)
-					}
+							serverInfo, err := browser.GetServerInfoWithTimeout(ip, port, cfg.RefreshInterval)
+							if err != nil {
+								updateMessage = fmt.Sprintf("%s: %s", markdown.WrapInFat("[DOWN]"), address)
+								log.Println(err)
+							} else {
+								updateMessage = formatServerInfo(serverInfo)
+							}
 
-					msg, err = ctx.EditMessage(channelID, msgID, updateMessage, nil, true)
-					if err != nil {
-						return err
-					}
+							msg, err = ctx.EditMessage(channelID, msgID, updateMessage, nil, true)
+							if err != nil {
+								log.Printf("could not edit message: %v\v", err)
+								return
+							}
 
-					bulletinBoard.UpdateMsgID(address, msg.ID)
-
-					return nil
-				})
+						},
+						channelID, msgID, ip, port)
 			}
 
+			scheduler.
+				StartAt(time.Now().Add(time.Second)).
+				StartAsync()
 			return nil
 		},
 	)
@@ -101,13 +129,13 @@ func toIPAndPort(address string) (string, int, error) {
 }
 
 type Bot struct {
-	Ctx *bot.Context
-	Cfg *Config
-	BB  *BulletinBoard
+	Ctx             *bot.Context
+	Cfg             *Config
+	AddressMsgIDMap map[string]discord.MessageID
 }
 
 func (b *Bot) Stop(event *gateway.MessageCreateEvent) (string, error) {
-	for _, msgID := range bulletinBoard.AddressMessageMap {
+	for _, msgID := range b.AddressMsgIDMap {
 		b.Ctx.DeleteMessage(b.Cfg.DiscordChannelID, msgID)
 	}
 	b.Ctx.DeleteMessage(event.ChannelID, event.Message.ID)
